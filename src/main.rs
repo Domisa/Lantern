b46f036e05ab1508f7533a77f34e22d2ed59731f
Dom Molsbarger <molsbarger.daniel@gmail.com>
2026-05-10 21:38:20 -0500

    feat: add basic axum server listening on port 8080

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    
+
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async {" Lantern is running"}));
+
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    println!("listening on port 8080");
+    axum::serve(listener, app).await.unwrap();
 }
