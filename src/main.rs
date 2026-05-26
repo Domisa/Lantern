b86d04f08e7e6522fecea00980d32557f623cd07
Dom Molsbarger <molsbarger.daniel@gmail.com>
2026-05-26 13:19:36 -0500

    struct currently unused, but fixed typo error and added more functions

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
-//added extract::path
-use axum::{Router, routing::get, extract::path};
+
+use axum::{Router, routing::get, extract::State, Json};
 use tokio::net::TcpListener;
+use serde::Deserialize;
+use sqlx::PgPool;
 
 #[derive(Deserialize)]
-struct Task {
-    date: String,
-    task: String,
-    summary: String,
+struct TaskPath {
+    id: i32,
 }
 
+async fn get_tasks(
+    State(_pool): State<PgPool>,
+) -> impl axum::response::IntoResponse {
+
+}
 // Taking a small break to better learn axum before continuing.
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async {" Lantern is running"}));
+
+    let pool = PgPool::connect(&std::env::var("DATABASE_URL").unwrap())
+        .await
+        .unwrap();
+
+    let app = Router::new()
+        .route("/tasks", get(get_tasks))
+        .with_state(pool);
 
     let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
     println!("listening on port 8080");
